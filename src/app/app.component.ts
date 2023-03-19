c397a8d80fb79f0ed892c2395daec39c35f6257a
denisselet <den8rr@gmail.com>
2023-03-19 15:28:35 +0200

    feat: completed intro-task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { SearchItem } from './search/search-item.model';
+import { default as data } from './data/results.json';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'youtubeClient';
+
+  items: SearchItem[] = data.items;
 }
---
+++ src/app/search/search-results/search-results.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+import { SearchItem } from '../search-item.model';
+
+@Component({
+  selector: 'app-search-results',
+  templateUrl: './search-results.component.html',
+  styleUrls: ['./search-results.component.scss']
+})
+export class SearchResultsComponent {
+  @Input() items: SearchItem[];
+}
